919577aaebc11aecbfedeee6a4c41e3d33a6a936
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-31 20:55:18 +0900

    7. Math functions

--- learn-py.py
+++ learn-py.py
@@ -1,8 +1,16 @@
-get_input = input("Enter a str: ")
-get_input_int = int(input("Enter a int: "))
-get_input_float = float(input("Enter a float: "))
-get_input_int += 1
-# get_input_float += 1.3
-print("Input 1: " + get_input)
-print("Input 2: " + str(get_input_int))
-print("Input 3: " + str(get_input_float))
+import math
+
+pi = math.pi
+x = 1
+y = 2
+z = 3
+
+print(pi)
+print(round(pi)) # round
+print(math.ceil(pi)) # round up
+print(math.floor(pi)) # round down
+print(abs(-pi)) # absolute value
+print(pow(pi, 2)) # power
+print(math.sqrt(pi)) # square root
+print(max(x, y, z))
+print(min(x, y, z))

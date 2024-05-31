7e40c4fe180bad82087f478cd2b8363ca098bba9
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-31 20:42:09 +0900

    5. Type casting

--- learn-py.py
+++ learn-py.py
@@ -1,12 +1,10 @@
-name = "bob"
+num = 3
 
-print(len(name))
-print(name.find("b"))
-print(name.capitalize()) # Capitalize the first letter
-print(name.upper())
-print(name.lower()) 
-print(name.isdigit()) # Check if all characters are digits
-print(name.isalpha()) # Check if all characters are alphabetic
-print(name.count("b"))
-print(name.replace("b", "l"))
-print(name*3)
+num_float = float(num)
+num_str = str(num)
+num_int = int(num)
+num_bool = bool(num)
+
+print(num_float, num_str, num_int, num_bool)
+print(type(num_float), type(num_str), type(num_int), type(num_bool))
+print(bool(0), bool(1), bool(2), bool(3), bool(4), bool(5), bool(6), bool(7), bool(8), bool(9), bool(10))

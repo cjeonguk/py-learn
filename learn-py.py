c10fcda4050cb4235f456bd6da92c3986a0ec500
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-31 20:49:42 +0900

    6. User input

--- learn-py.py
+++ learn-py.py
@@ -1,10 +1,8 @@
-num = 3
-
-num_float = float(num)
-num_str = str(num)
-num_int = int(num)
-num_bool = bool(num)
-
-print(num_float, num_str, num_int, num_bool)
-print(type(num_float), type(num_str), type(num_int), type(num_bool))
-print(bool(0), bool(1), bool(2), bool(3), bool(4), bool(5), bool(6), bool(7), bool(8), bool(9), bool(10))
+get_input = input("Enter a str: ")
+get_input_int = int(input("Enter a int: "))
+get_input_float = float(input("Enter a float: "))
+get_input_int += 1
+# get_input_float += 1.3
+print("Input 1: " + get_input)
+print("Input 2: " + str(get_input_int))
+print("Input 3: " + str(get_input_float))

431da8ff4ca7511d3efe56246f451714ecdd444e
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-06 00:24:26 +0900

    10. Logical operators

--- learn-py.py
+++ learn-py.py
@@ -1,10 +1,19 @@
-choice = "paper"
+age = 13
 
-if choice == "rock":
-    print("You chose rock.")
-elif choice == "paper":
-    print("You chose paper.")
-elif choice == "scissors":
-    print("You chose scissors.")
-else:
-    print("Invalid choice.")
+if age >= 18 and age <= 65:
+    print('adult')
+
+elif age <= 18 and age >= 13:
+    print('teenager')
+
+elif age < 13 and age >= 3:
+    print('child')
+
+elif not(age >= 3):
+    print('baby')
+
+elif age > 65 and age < 100:
+    print('elderly')
+
+elif age >= 100 or age < 0:
+    print('centenarian or not born yet')

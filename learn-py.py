e7def343846d05a0e252ab475c33b3a4f9376701
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-12 15:13:03 +0900

    11. While loops

--- learn-py.py
+++ learn-py.py
@@ -1,19 +1,5 @@
-age = 13
+a = "start"
 
-if age >= 18 and age <= 65:
-    print('adult')
-
-elif age <= 18 and age >= 13:
-    print('teenager')
-
-elif age < 13 and age >= 3:
-    print('child')
-
-elif not(age >= 3):
-    print('baby')
-
-elif age > 65 and age < 100:
-    print('elderly')
-
-elif age >= 100 or age < 0:
-    print('centenarian or not born yet')
+while a != "exit":
+    a = input("Enter a command: ")
+    print("You entered", a)

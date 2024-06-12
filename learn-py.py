ee5ee1c2a0a27c9044bf2c0e2b7af17989908c6e
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 01:54:57 +0900

    30. Exception handling

--- learn-py.py
+++ learn-py.py
@@ -1,18 +1,9 @@
-import random
+try:
+    a = int(input("Enter a number: "))
 
-x = random.randint(1, 100)
-print(x)
+except ValueError:
+    print("You entered an invalid number")
+    a = 0
 
-y = random.random()
-
-print(y)
-
-z = random.choice(["apple", "banana", "cherry"])
-
-print(z)
-
-cities = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"]
-
-random.shuffle(cities)
-
-print(cities)
+finally:
+    print("The number is: ", a)

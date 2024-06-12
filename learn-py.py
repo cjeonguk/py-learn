74cee116a1511e056f289754807c9b001450f2f2
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 01:47:51 +0900

    29. Random numbers

--- learn-py.py
+++ learn-py.py
@@ -1,4 +1,18 @@
-def func(**kwargs):
-    print(kwargs)
+import random
 
-func(a = 1, b = 2, c = 3)
+x = random.randint(1, 100)
+print(x)
+
+y = random.random()
+
+print(y)
+
+z = random.choice(["apple", "banana", "cherry"])
+
+print(z)
+
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"]
+
+random.shuffle(cities)
+
+print(cities)

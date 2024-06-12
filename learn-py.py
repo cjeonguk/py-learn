c523ca46c9fd7c0ad8b99cf4dfeb1a11a4c89058
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-12 23:29:45 +0900

    16. 2D lists

--- learn-py.py
+++ learn-py.py
@@ -1,16 +1,5 @@
 company = ['Google', 'Facebook', 'Amazon', 'Microsoft', 'Apple']
-print(company)
 
-company.append('IBM')
-company.remove('Amazon')
-company.pop(2)
+l = [company, [1, 2, 3, 4, 5]]
 
-company.insert(2, 'Amazon')
-
-company.sort()
-
-# company.clear()
-
-
-for i in company:
-    print(i)
+print(l[0][1])  # Facebook

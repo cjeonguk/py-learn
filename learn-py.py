96ef713ba24018333de206b91de3507b5ff8a6e5
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 01:06:51 +0900

    21. Functions

--- learn-py.py
+++ learn-py.py
@@ -1,15 +1,5 @@
-d = {"a": 1, "b": 2, "c": 3}
+def func(name):
+    print(f"Hello from a {name}")
 
-print(d.get("a"))
 
-print(d.keys())
-
-print(d.values())
-
-print(d.items())
-
-d.update({"d": 4})
-d.update({"a": 5})
-
-for key, value in d.items():
-    print(key, value)
+func("function")

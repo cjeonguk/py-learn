5780660d71e198f68b14af2accc986e533f87660
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 01:30:29 +0900

    25. Variable scope

--- learn-py.py
+++ learn-py.py
@@ -1,7 +1,12 @@
+var = 20
 def func(name = "default"):
+    var = 10
     print(f"Hello from a {name}")
+    print(var)
     return f"Hi {name}"
 
 
 print(func("function"))
 print(func())
+
+print(var)

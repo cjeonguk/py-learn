e5c609d4ee8596f0b5f16b9225d8eec2322c8393
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 01:34:07 +0900

    26. *args

--- learn-py.py
+++ learn-py.py
@@ -1,12 +1,4 @@
-var = 20
-def func(name = "default"):
-    var = 10
-    print(f"Hello from a {name}")
-    print(var)
-    return f"Hi {name}"
+def func(*args):
+    print(args)
 
-
-print(func("function"))
-print(func())
-
-print(var)
+func(1, 2, 3)

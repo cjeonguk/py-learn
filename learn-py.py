e701b6fae368ede2b145a43b44b88efee2624a16
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-13 02:11:32 +0900

    32. read file

--- learn-py.py
+++ learn-py.py
@@ -1,9 +1,2 @@
-try:
-    a = int(input("Enter a number: "))
-
-except ValueError:
-    print("You entered an invalid number")
-    a = 0
-
-finally:
-    print("The number is: ", a)
+with open('demofile.txt') as f:
+    print(f.read())

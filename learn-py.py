18dc623c39844c0fc6b57813dd1bba9721a66e90
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 21:14:42 +0900

    57. filter

--- learn-py.py
+++ learn-py.py
@@ -5,7 +5,6 @@ store = [
     ("socks", 10.00)
 ]
 
-to_wons = lambda data: (data[0], data[1] * 1000)
-store_wons = list(map(to_wons, store))
+filtered = list(filter(lambda data: data[1] >= 20, store))
 
-print(store_wons)
+print(filtered)

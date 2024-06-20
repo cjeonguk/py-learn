53b0913a169c355e13e913e34195483d16d067ba
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 21:12:58 +0900

    56. map

--- learn-py.py
+++ learn-py.py
@@ -1,17 +1,11 @@
-template_list_complex = [
-    {
-        "name": "learn-py.py",
-        "path": "learn-py.py",
-        "type": "file",
-        "content": "print('Hello, Python!')",
-    },
-    {
-        "name": "mod.py",
-        "path": "mod.py",
-        "type": "file",
-        "content": "...",
-    },
+store = [
+    ("shirt", 20.00),
+    ("pants", 25.00),
+    ("jacket", 50.00),
+    ("socks", 10.00)
 ]
 
-# template_list_complex.sort(key=lambda x: x["name"], reverse=True)
-print(sorted(template_list_complex, key=lambda x: x["name"], reverse=True))
+to_wons = lambda data: (data[0], data[1] * 1000)
+store_wons = list(map(to_wons, store))
+
+print(store_wons)

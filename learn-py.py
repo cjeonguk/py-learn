556abd32bee1d488f22a0803a79070b39e74df9d
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 20:30:10 +0900

    53. Higher inner functions

--- learn-py.py
+++ learn-py.py
@@ -6,3 +6,14 @@ a(print, "c")
 d = print
 
 d("e")
+
+def devisor(x):
+    def inner(y):
+        return x / y
+    return inner
+
+devide = devisor(10)(5)
+
+devide2 = devisor(10)
+
+print(devide, devide2(5))

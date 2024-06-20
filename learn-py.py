1d569f2c5b662af1e78371db55fd312dcba968d0
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 20:23:14 +0900

    51. Warlus operator

--- learn-py.py
+++ learn-py.py
@@ -1,18 +1,1 @@
-import mod
-
-# mod.Human('Alice').say_hello()
-
-# print(mod.Human('Bob').is_alive)
-
-mod.Human.is_alive = False
-
-print(mod.Student('Charlie', 3).is_alive)
-
-s = mod.Student('David', 1)
-
-s.say_hello()\
-    .study()
-
-mod.rename(s, 'Eve')
-
-print(s.name)
+print(a := 1)

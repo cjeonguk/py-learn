b08bae244649b188e6f18814a073cf07cd988b19
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 16:54:26 +0900

    41. Class variables

--- learn-py.py
+++ learn-py.py
@@ -1,3 +1,9 @@
 import mod
 
 mod.Human('Alice').say_hello()
+
+print(mod.Human('Bob').is_alive)
+
+mod.Human.is_alive = False
+
+print(mod.Human('Charlie').is_alive)
--- mod.py
+++ mod.py
@@ -2,5 +2,7 @@ class Human:
     def __init__(self, name):
         self.name = name
 
+    is_alive = True
+
     def say_hello(self):
         print(f'Hello, my name is {self.name}')

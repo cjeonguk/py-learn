c1dc9c5a15eb48999d89706f833002f4882624ed
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-20 21:09:20 +0900

    55. Sort

--- learn-py.py
+++ learn-py.py
@@ -1,4 +1,17 @@
-add = lambda x, y: x + y
-multiply = lambda x, y: x * y
+template_list_complex = [
+    {
+        "name": "learn-py.py",
+        "path": "learn-py.py",
+        "type": "file",
+        "content": "print('Hello, Python!')",
+    },
+    {
+        "name": "mod.py",
+        "path": "mod.py",
+        "type": "file",
+        "content": "...",
+    },
+]
 
-print(multiply(add(2, 3), 5))
+# template_list_complex.sort(key=lambda x: x["name"], reverse=True)
+print(sorted(template_list_complex, key=lambda x: x["name"], reverse=True))

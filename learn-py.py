46bb3bd1412f6313418b519594867c63466023a5
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-21 00:22:46 +0900

    66. multiprocessing

--- learn-py.py
+++ learn-py.py
@@ -1,14 +1,18 @@
-import threading
+from multiprocessing import Process, cpu_count
 import time
 
-def deamon():
-    print('Starting')
-    time.sleep(2)
-    print('Exiting')
+def foo(name):
+    print('hello', name)
 
-x = threading.Thread(target=deamon, daemon=True)
+def main():
+    print(f"Number of CPU cores: {cpu_count()}")
+    x = Process(target=foo, args=("10k",))
 
-x.start()
+    x.start()
+    x.join()
 
-print('Main thread exiting')
-print(x.daemon)
+    print(time.perf_counter())
+
+
+if __name__ == '__main__':
+    main()
